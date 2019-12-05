08b93cec2e13fab411ae202f8514b9a9d17a84c6
Samuel Nwoye <nwoyesamuelc@gmail.com>
2019-12-05 20:26:33 +0100

    refactored  master.sh

--- provisioner/master.sh
+++ provisioner/master.sh
@@ -5,10 +5,10 @@ echo "Configuring the Master Node ..."
 echo IP_ADDR=`hostname -I | awk '{print $2}'`
 
 # get the hostname of the master
-echo HOST_NAME=$(hostname -s)
+echo HOSTNAME=$(hostname -s)
 
 # initialize the cluster control plane on the master no
-sudo kubeadm init --apiserver-advertise-address=192.168.56.7 --apiserver-cert-extra-sans=192.168.56.7  --node-name=$HOST_NAME --pod-network-cidr=172.16.0.0/16
+sudo kubeadm init --apiserver-advertise-address=192.168.56.7 --apiserver-cert-extra-sans=192.168.56.7  --node-name=$HOSTNAME --pod-network-cidr=172.16.0.0/16
 
 # configure kube config
 mkdir -p $HOME/.kube 
@@ -20,10 +20,4 @@ export KUBECONFIG=$HOME/.kube/config
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
 # make the master node workload schedulable
-kubectl taint nodes $HOST_NAME node-role.kubernetes.io/master-
-
-# create and store a new cluster join token
-# sudo kubeadm token create --print-join-command > /vagrant/join.sh
-# scp /vagrant/join.sh vagrant@192.168.56.11:/vagrant/
-
-# vagrant provision --provision-with shell
+kubectl taint nodes $HOSTNAME node-role.kubernetes.io/master-
